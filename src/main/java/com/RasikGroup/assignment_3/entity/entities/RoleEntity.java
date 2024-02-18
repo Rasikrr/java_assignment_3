65a808ebc9395bcca62b7cb20de983d0c98bd03a
Rasikrr <rs.t.95@mail.ru>
2024-02-18 18:40:12 +0600

    UserEntity and RoleEntity were added

---
+++ src/main/java/com/RasikGroup/assignment_3/entity/entities/RoleEntity.java
@@ -0,0 +1,29 @@
+package com.RasikGroup.assignment_3.entity.entities;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+
+import java.util.Set;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "t_role")
+public class RoleEntity implements GrantedAuthority {
+    @Id
+    private Long id;
+    private String name;
+    @Transient
+    @ManyToMany(mappedBy = "roles")
+    private Set<UserEntity> userEntities;
+
+
+    @Override
+    public String getAuthority() {
+        return getName();
+    }
+}
---
+++ src/main/java/com/RasikGroup/assignment_3/entity/entities/UserEntity.java
@@ -0,0 +1,54 @@
+package com.RasikGroup.assignment_3.entity.entities;
+
+import jakarta.persistence.*;
+import lombok.Data;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.Set;
+
+@Data
+@Entity
+public class UserEntity implements UserDetails {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String first_name;
+    private String last_name;
+    private String username;
+    private String password;
+    @Transient
+    private String passwordConfirm;
+
+    @ManyToMany(fetch = FetchType.EAGER)
+    private Set<RoleEntity> roles;
+
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return getRoles();
+    }
+
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+}
